b2c27a0844598b0840448903b17118cb8d821fb7
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2024-07-01 02:01:53 +0530

    test(core): added more tests

--- packages/core/src/__tests__/core.spec.ts
+++ packages/core/src/__tests__/core.spec.ts
@@ -28,6 +28,7 @@ describe("health check", () => {
     );
 
     await request(app).get("/").expect(503, "B");
+    app.close();
   });
 
   it("delayed handling with promise success", async () => {
@@ -41,7 +42,7 @@ describe("health check", () => {
       {
         healthCheck: async () => false,
         healthCheckInterval: 1,
-        pressureHandler: async (c, type, value) => {
+        pressureHandler: async () => {
           await wait(250);
           throw new HTTPException(503, { message: "B" });
         },
@@ -49,6 +50,7 @@ describe("health check", () => {
     );
 
     await request(app).get("/").expect(503, "B");
+    app.close();
   });
 
   it("no handling", async () => {
@@ -62,21 +64,16 @@ describe("health check", () => {
       {
         healthCheck: async () => false,
         healthCheckInterval: 1,
-        pressureHandler: async (c, type, value) => {},
+        pressureHandler: () => {},
       },
     );
 
     await request(app).get("/").expect(200, "Hi there!");
+    app.close();
   });
 
-  it.skip("interval reentrance", async (t) => {
-    vi.useFakeTimers();
-    t.onTestFinished(() => {
-      vi.useRealTimers();
-      vi.restoreAllMocks();
-    });
-
-    const healthCheckInterval = 500;
+  it("interval reentrance", async (t) => {
+    const healthCheckInterval = 100;
 
     const healthCheck = vi.fn(async () => {
       await wait(healthCheckInterval * 2);
@@ -100,12 +97,12 @@ describe("health check", () => {
     expect(healthCheck).toBeCalledTimes(1);
 
     // wait until next execution
-    vi.advanceTimersByTime(healthCheckInterval);
+    await wait(healthCheckInterval);
 
     // scheduled by the timer
     expect(healthCheck).toBeCalledTimes(2);
 
-    vi.advanceTimersByTime(healthCheckInterval);
+    await wait(healthCheckInterval);
 
     // still running the previous invocation
     expect(healthCheck).toBeCalledTimes(2);
@@ -113,9 +110,44 @@ describe("health check", () => {
     // wait until the last call resolves and schedules another invocation
     expect(healthCheck).toReturn();
 
-    vi.advanceTimersByTime(healthCheckInterval);
+    await wait(healthCheckInterval * 2);
 
     // next timer invocation
     expect(healthCheck).toBeCalledTimes(3);
+    app.close();
   });
+});
+
+it.skip("event loop delay", async () => {
+  const app = underPressure(
+    (handlers) =>
+      createAdaptorServer(
+        createServer({
+          middleware: handlers,
+        }),
+      ),
+    {
+      maxEventLoopDelay: 1,
+      pressureHandler: (c, type, value) => {
+        expect(type).toEqual(PressureType.EVENT_LOOP_DELAY);
+        expect(value).gt(1);
+
+        throw new HTTPException(503, { message: "B" });
+      },
+    },
+  );
+
+  await request(app).get("/").expect(503, "B");
+  app.close();
+});
+
+it.todo("heap bytes");
+it.todo("rss bytes");
+it.todo("event loop utilization");
+it.todo("event loop delay (NaN)");
+
+describe("pressureHandler on route", () => {
+  test.todo("simple");
+  test.todo("delayed handling with promise error");
+  test.todo("no handling");
 });
--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -4,24 +4,17 @@ import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: "../../node_modules/.vite/packages/under-pressure",
-
+  cacheDir: "../../node_modules/.vite/packages/core",
   plugins: [nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
   test: {
     watch: false,
     globals: true,
-    cache: { dir: "../../node_modules/.vitest/packages/under-pressure" },
+    cache: { dir: "../../node_modules/.vitest/packages/core" },
     environment: "node",
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     reporters: ["default"],
     coverage: {
-      reportsDirectory: "../../coverage/packages/under-pressure",
+      reportsDirectory: "../../coverage/packages/core",
       provider: "v8",
     },
   },
